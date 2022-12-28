3808bbc7e4524b25a8b07715f7bc5f646f262636
pdj1183 <pdj1183@gmail.com>
2022-12-28 16:43:17 -0700

    Initial Upload
    Navbar and basic header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {Navigation} from "./components/Navigation";
+import {Banner} from "./components/Banner";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Navigation/>
+        <Banner/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,62 @@
+import {useState, useEffect} from "react";
+import {Container, Col, Row} from "react-bootstrap";
+import {ArrowRightCircle} from "react-bootstrap-icons";
+import signature from "../assets/img/Signature.svg"
+
+export const Banner = () =>{
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false)
+    const carousel = ["Software Engineer", "Cyber-Security Student"];
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100)
+    const period = 200;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta)
+
+        return () => { clearInterval(ticker)};
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % carousel.length;
+        let fullText = carousel[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta /2)
+        }
+
+        if (!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        }else if (isDeleting && updatedText === ''){
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+
+    return(
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Welcome to my Portfolio</span>
+                        <h1>{'Hi I\'m Phillip Johnson '}<span className="wrap"> {text} </span></h1>
+                        <p>alsdflasdhufhasldhfjahsdhf;kasd;klfj;kasd;kfh   dsa;kfha.s dkhjfasd;f ;a;kj;l df;la sd;lhf; ;h;sdjhflkjahsdljfh lkashd fkjhasldkhfl akjsdlfjlkajhsdl fkhasldkfh</p>
+                        <button onClick={() => console.log('connect')}> Let's Connect <ArrowRightCircle size={25}/> </button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={signature} alt="Header Img"/>
+                    </Col>
+
+                </Row>
+            </Container>
+        </section>
+
+    )
+}
