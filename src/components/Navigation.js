3038160e219902e09679d02d911f8fe9ff960045
pdj1183 <pdj1183@gmail.com>
2022-12-29 19:24:35 -0700

    Created a splash page and small edits to navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {Button} from "react-bootstrap";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import logo from '../assets/img/Signature1.svg'
 import navIcon1 from '../assets/img/nav-icon1.svg'
 import navIcon2 from '../assets/img/nav-icon2.svg'
@@ -10,28 +10,13 @@ import navIcon3 from '../assets/img/nav-icon3.svg'
 
 export  const Navigation = () =>{
     const [activeLink, setActiveLink] = useState('home');
-    const [scrolled, setScrolled] = useState(false);
-
-    useEffect(() => {
-        const onScroll = () => {
-            if (window.scrollY > 50) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
-        }
-
-        window.addEventListener("scroll", onScroll);
-
-        return () => window.removeEventListener("scroll", onScroll);
-    }, [])
 
     const onUpdateActiveLink = (value) => {
         setActiveLink(value);
     }
 
     return (
-        <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
+        <Navbar expand="lg" className={"scrolled"}>
             <Container>
                 <Navbar.Brand href="#home">
                     <img src={logo} class="img-fluid" alt="Logo" />
