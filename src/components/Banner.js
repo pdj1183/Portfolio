587cafb341fe0a296d8c214c8832e61debe31ccc
pdj1183 <pdj1183@gmail.com>
2022-12-30 20:14:47 -0700

    Small improvements across the site

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import {Container, Col, Row} from "react-bootstrap";
 import {ArrowRightCircle} from "react-bootstrap-icons";
 import Typical from "react-typical"
+import portrait from '../assets/img/Snapchat-1319243128.jpg'
 
 
 export const Banner = () =>{
@@ -9,28 +10,26 @@ export const Banner = () =>{
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <h1>{'Well '}</h1>
-                        <h2 > I'm a
+                        <h1>{'Hello There!'}</h1>
+                        <h2 > I'm
                                 <Typical
                                 loop={Infinity}
                                 wrapper="b"
                                 steps={[
-                                    ' CSU Student',
+                                    ' a CSU Student',
                                     1000,
-                                    'n Adventurer',
+                                    ' an Adventurer',
                                     1000,
                                     ' Motivated to Learn',
                                     1000
                                 ]}
                                 />
                         </h2>
-                        <p>{'\n'}alsdflasdhufhasldhfjahsdhf;kasd;klfj;kasd;kfh   dsa;kfha.s dkhjfasd;f ;a;kj;l df;la sd;lhf; ;h;sdjhflkjahsdljfh lkashd fkjhasldkhfl akjsdlfjlkajhsdl fkhasldkfh</p>
+                        <p>{'\n'}alsdflasdhufhasldhfjahsdhf; kasd;klfj;kasd;kfh   dsa;kfha.s dkhjfasd;f ;a;kj;l df;la sd;lhf; ;h;sdjhflkjahsdljfh lkashd fkjhasldkhfl akjsdlfjlkajhsdl fkhasldkfh</p>
                         <button onClick={() => console.log('connect')}> Let's Connect <ArrowRightCircle size={25}/> </button>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
-                        <h3 className='text-center'>
-                            Welcome to my Portfolio
-                        </h3>
+                        <img src={portrait} alt="portrait"/>
                     </Col>
 
                 </Row>
