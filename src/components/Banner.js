bfe731916112a61060dfd343bcd558b03b21b27d
Phillip Johnson <pdj1183@gmail.com>
2023-11-06 10:35:37 -0700

    Update old info and add new projects

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,48 +1,63 @@
-import {Container, Col, Row} from "react-bootstrap";
-import Typical from "react-typical"
-import portrait from '../assets/img/Snapchat-1319243128.jpg'
+import { Container, Col, Row } from "react-bootstrap";
+import Typical from "react-typical";
+import portrait from "../assets/img/Portrait.JPG";
 
-
-export const Banner = () =>{
-    return(
+export const Banner = () => {
+    return (
         <section className="banner" id="home">
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <h1>{'Hello There!'}</h1>
-                        <h2 > I'm
-                                <Typical
+                        <h1>{"Hello There!"}</h1>
+                        <h2>
+                            {" "}
+                            I'm
+                            <Typical
                                 loop={Infinity}
                                 wrapper="b"
                                 steps={[
-                                    ' a CSU Student',
+                                    " a CSU Student",
                                     1000,
-                                    ' an Adventurer',
+                                    " a Hardware Hacker",
                                     1000,
-                                    ' a Coder',
+                                    " a Coder",
                                     1000,
-                                    ' Motivated to Learn',
-                                    1000
+                                    " Always Curious",
+                                    1000,
                                 ]}
-                                />
+                            />
                         </h2>
-                        <p>{'\n'}
-                            I am a third year Computer Science undergraduate with a concentration in Networking and Cyber-security as well as a minor in Mathematics.
-                            I'm interested to learn about all corners of technology.
-                            I love building new apps and have an eye for appealing user experiences.
-                            The most impactful class I have taken is Introduction to Software Engineering cs314.
-                            It taught me important skills about working as part of a 5 person team on a large continuous project.
-                            We created a mobile trip itinerary that was based of off a given a base application.
-                            In the first week we had to analyze the code and begin adding our own improvements.
+                        <p className="summary">
+                            {"\n"}I am a motivated fourth-year Computer Science
+                            undergraduate with a concentration in Cybersecurity
+                            and Networking, along with a minor in Mathematics.
+                            As a dedicated research assistant, I have had the
+                            opportunity to delve into the fascinating realms of
+                            hardware reverse engineering. Working under the
+                            guidance of esteemed professionals, I've conducted
+                            in-depth research and developed experience in
+                            analyzing and manipulating Blackbox devices,
+                            implementing innovative attacks to alter their
+                            output without raising suspicion. Through this
+                            experience, I strengthened my skills in information
+                            gathering, PCB schematic recreation, IoT device
+                            application creation, and hardware measurements. I
+                            have performing controlled experiments, collecting
+                            hardware and digital data, and demonstrating
+                            statistical significance to expose vulnerabilities.
+                            With a drive to explore cutting-edge technologies, I
+                            am passionate about building secure and robust
+                            systems to safeguard against cyber threats. Welcome
+                            to my portfolio, where I showcase my dedication to
+                            learning Computer Science through my projects and
+                            accomplishments.
                         </p>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
-                        <img src={portrait} alt="portrait"/>
+                        <img src={portrait} alt="portrait" />
                     </Col>
-
                 </Row>
             </Container>
         </section>
-
-    )
-}
+    );
+};
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,50 +5,52 @@ import projImg2 from "../assets/img/20210222_224359.jpg";
 import projImg3 from "../assets/img/Screenshot 2023-01-12 at 7.06.49 PM.png";
 import projImg4 from "../assets/img/Screenshot 2023-01-19 at 4.13.33 PM.png";
 import projImg5 from "../assets/img/CleanShot 2023-03-13 at 21.55.53@2x.png";
-import colorSharp2 from "../assets/img/color-sharp2.png";
+import projImg6 from "../assets/img/Screenshot 11052023HuE0jbA6@2x.png";
+// import colorSharp2 from "../assets/img/color-sharp2.png";
 import TrackVisibility from "react-on-screen";
 import virtualization from "../assets/img/D3 Final.pdf";
+import HCI from "../assets/img/Final Project.pdf";
 
 export const Projects = () => {
     const projects = [
         {
             title: "Mobile Trip Itinerary ",
             description:
-                "Team project using SCRUM and github." +
-                "We started with a base app and added extra functionality to make the app easier to use." +
-                " I created some app modals for pages to optimize the trip and search a database to find airports by name." +
-                " I use mySQL to query the database that was hosted on our school network.",
+                "Team project using SCRUM and github. We started with a base app and added extra functionality to make the app easier to use. I created some app modals for pages to optimize the trip and search a database to find airports by name. I use mySQL to query the database that was hosted on our school network.",
             imgUrl: projImg1,
         },
         {
             title: "Sound Reactive Light Strip",
             description:
-                "I created a sound responsive LED light strip using an ESP32 arduino as the controller." +
-                " I used fastLED to control the LED strip and arduinoFFT to analyze the sound from a microphone.",
+                "I created a sound responsive LED light strip using an ESP32 arduino as the controller. I used fastLED to control the LED strip and arduinoFFT to analyze the sound from a microphone.",
             imgUrl: projImg2,
         },
         {
             title: "IEEE Research Paper",
             description:
-                "Group research paper on the benefits of virtualization in the case of natural disaster." +
-                "",
+                "Group research paper on the benefits of virtualization in the case of natural disaster.",
             imgUrl: projImg3,
             page: virtualization,
         },
         {
-            title: "Personal Portfolio Page",
+            title: "This Portfolio Page",
             description:
-                "A Personal Portfolio I made using ReactJS, NPM, and React Three Fiber for the cover page." +
-                "I am hosting the page using GitHub pages.",
+                "A Personal Portfolio I made using ReactJS, NPM, and React Three Fiber for the cover page. I am hosting the page using GitHub pages.",
             imgUrl: projImg4,
         },
         {
             title: "Photography Portfolio Page",
             description:
-                "A Photography Portfolio I made using ReactJS, NPM, and Parallax for my friend Cosmo." +
-                "Hosted on GitHub pages, but need to optimize the image loading to improve site performance.",
+                "A Photography Portfolio I made using ReactJS, NPM, and Parallax for my friend Cosmo. Hosted on GitHub pages, but need to optimize the image loading to improve site performance.",
             imgUrl: projImg5,
             page: "https://pdj1183.github.io/Cosmo-Portfolio/",
+        },
+        {
+            title: "Effects of Entertainment on Knowledge Retentuion",
+            description:
+                "Group Research paper focused on discovering what effect a study tool's entertainment had on knowledge retention. This required a custom study tool made with Unreal Engine 5, along with an experiment using this custom tool. Statistical analysis was used to determine the effect our tool had on knowledgre retnetion. ",
+            imgUrl: projImg6,
+            page: HCI,
         },
     ];
 
@@ -89,11 +91,11 @@ export const Projects = () => {
                     </Col>
                 </Row>
             </Container>
-            <img
+            {/* <img
                 className="background-image-right"
                 src={colorSharp2}
                 alt="bg-gradient"
-            />
+            /> */}
         </section>
     );
 };
